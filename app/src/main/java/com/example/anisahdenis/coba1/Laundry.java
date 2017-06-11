ef23daca2aa8de80eab0d2c0766f62f51d1708ab
MEITAPTR\meita <meitaputria@gmail.com>
2017-06-11 14:46:42 +0700

    tambah beberapa variable

--- app/src/main/java/com/example/anisahdenis/coba1/Laundry.java
+++ app/src/main/java/com/example/anisahdenis/coba1/Laundry.java
@@ -8,6 +8,8 @@ public class Laundry {
     private String laundry_id;
     private String nama;
     private String alamat;
+    private String telpon;
+    private String website;
     private double latitude;
     private double longitude;
 
@@ -15,12 +17,14 @@ public class Laundry {
 
     }
 
-    public Laundry(String laundry_id, String nama, String alamat, double latitude, double longitude) {
+    public Laundry(String laundry_id, String nama, String alamat, double latitude, double longitude, String telpon, String website) {
         this.laundry_id = laundry_id;
         this.nama = nama;
         this.alamat = alamat;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.telpon = telpon;
+        this.website = website;
     }
 
     public String getLaundry_id() {
@@ -41,5 +45,21 @@ public class Laundry {
 
     public double getLongitude() {
         return longitude;
+    }
+
+    public String getTelpon() {
+        return telpon;
+    }
+
+    public void setTelpon(String telpon) {
+        this.telpon = telpon;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
     }
 }
